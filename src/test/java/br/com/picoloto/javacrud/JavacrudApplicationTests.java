363459a5ec9ad56273241ad17973a906c9ceea12
picoloto <picolotoleandro@gmail.com>
2021-06-07 19:25:12 -0300

    Criação do Projeto Java

---
+++ src/test/java/br/com/picoloto/javacrud/JavacrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.picoloto.javacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
